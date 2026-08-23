4b3d505a05a1dcb49ba66ad3487006f8ff78e510
Siddharth Mehta <siddharthmehta@Siddharths-MacBook-Air.local>
2026-08-23 10:27:57 +1000

    Publish beckythebat.com static site

--- vicky-the-bat-mobile/scripts/build-web.mjs
+++
@@ -1,6 +0,0 @@
-import { mkdir, copyFile } from 'node:fs/promises';
-
-await mkdir('www', { recursive: true });
-await copyFile('source/vicky.html', 'www/index.html');
-
-console.log('Built Becky the Bat web bundle from source/vicky.html');
